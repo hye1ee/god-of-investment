275d9ac4f398690f50e1d7fee1759bac07a338fb
hye1ee <hyewon0809@kaist.ac.kr>
2023-01-16 18:12:57 +0900

    [feat] Implement routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+
 import "./index.css";
+import Header from "./pages/components/Header";
+import Login from "./pages/login/index";
+import Home from "./pages/home/index";
+import Detail from "./pages/detail/index";
+import Issues from "./pages/issues/index";
+import Search from "./pages/search/index";
+import Simulation from "./pages/simulation/index";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/detail" element={<Detail />} />
+        <Route path="/issues" element={<Issues />} />
+        <Route path="/search" element={<Search />} />
+        <Route path="/simulation" element={<Simulation />} />
+        <Route path="/*" element={<Navigate replace to="/home" />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
