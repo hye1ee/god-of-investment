2ee2ed5ce69a87d20a75725db7b0018f787df6d3
hye1ee <hyewon0809@kaist.ac.kr>
2023-02-01 21:52:31 +0900

    [feat] Implement filter tab location part

--- src/pages/home/components/filter/LocationFilter.tsx
+++ src/pages/home/components/filter/LocationFilter.tsx
@@ -1,17 +1,22 @@
+import { useState } from "react";
 import { Wrapper } from "../../../components/Wrapper";
 import Button from "../../../components/Button";
-import { BorderRow } from "../../../components/Border";
+import { BorderColumn, BorderRow } from "../../../components/Border";
+import { locationNames } from "../../../../utils/constants";
+import { RegularText } from "../../../components/Text";
 
 export default () => {
+  const [location, setLocation] = useState(["서울시", "종로구"]);
+
   const onLocationFilter = () => {
     console.log("onLocationFilter button clicked");
   };
 
   return (
     <Wrapper direction="column" width="full">
-      <Wrapper direction="row" width="full" height={59}></Wrapper>
+      <LocationTitle location={location} />
       <BorderRow width={1} color="grayLight" />
-      <Wrapper direction="row" width="full" height={288}></Wrapper>
+      <LocationSelector location={location} setLocation={setLocation} />
       <BorderRow width={1} color="grayLight" />
 
       <Wrapper direction="row" width="full" height={75} center={true}>
@@ -30,6 +35,116 @@ export default () => {
         />
       </Wrapper>
       <BorderRow width={1} color="grayLight" />
+    </Wrapper>
+  );
+};
+
+const LocationTitle = ({ location }: { location: string[] }) => {
+  return (
+    <Wrapper direction="row" width="full" height={59}>
+      <Wrapper
+        direction="row"
+        width={190}
+        height="full"
+        center={true}
+        color="purpleBright"
+      >
+        <RegularText size={14} absolute={true} left={24}>
+          {location[0]}
+        </RegularText>
+      </Wrapper>
+      <Wrapper
+        direction="row"
+        width={190}
+        height="full"
+        center={true}
+        color="purpleBright"
+      >
+        <RegularText size={14} absolute={true} left={24}>
+          {location[1]}
+        </RegularText>
+      </Wrapper>
+    </Wrapper>
+  );
+};
+
+const LocationSelector = ({
+  location,
+  setLocation,
+}: {
+  location: string[];
+  setLocation: React.Dispatch<React.SetStateAction<string[]>>;
+}) => {
+  return (
+    <Wrapper direction="row" width="full" height={288}>
+      <Wrapper direction="column" width={94.25} height="full">
+        {Object.keys(locationNames)
+          .slice(0, 9)
+          .map((name) => (
+            <LocationName
+              active={name === location[0]}
+              name={name}
+              onClick={() => setLocation([name, locationNames[name][0]])}
+            />
+          ))}
+      </Wrapper>
+      <BorderColumn width={1} color="grayLight" />
+      <Wrapper direction="column" width={94.25} height="full">
+        {Object.keys(locationNames)
+          .slice(9)
+          .map((name) => (
+            <LocationName
+              active={name === location[0]}
+              name={name}
+              onClick={() => setLocation([name, locationNames[name][0]])}
+            />
+          ))}
+      </Wrapper>
+      <BorderColumn width={1} color="grayLight" />
+      <Wrapper direction="column" width={94.25} height="full">
+        {locationNames[location[0]].slice(0, 9).map((name) => (
+          <LocationName
+            active={name === location[1]}
+            name={name}
+            onClick={() => setLocation((location) => [location[0], name])}
+          />
+        ))}
+      </Wrapper>
+      <BorderColumn width={1} color="grayLight" />
+      <Wrapper direction="column" width={94.25} height="full">
+        {locationNames[location[0]].slice(9).map((name) => (
+          <LocationName
+            active={name === location[1]}
+            name={name}
+            onClick={() => setLocation((location) => [location[0], name])}
+          />
+        ))}
+      </Wrapper>
+    </Wrapper>
+  );
+};
+
+const LocationName = ({
+  active,
+  name,
+  onClick,
+}: {
+  active: boolean;
+  name: string;
+  onClick: () => void;
+}) => {
+  return (
+    <Wrapper
+      direction="row"
+      width="full"
+      height={32}
+      color={active ? "purpleLight" : "white"}
+      center={true}
+      onClick={onClick}
+    >
+      <RegularText color="gray" size={13} absolute={true} left={19}>
+        {name}
+      </RegularText>
     </Wrapper>
   );
 };
