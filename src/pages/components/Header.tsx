f9098f5e66fc1eff990ffa4d2a8e49c821ef02fb
hye1ee <hyewon0809@kaist.ac.kr>
2023-03-07 16:03:22 +0900

    [feat] Implement new design to header partially

--- src/pages/components/Header.tsx
+++ src/pages/components/Header.tsx
@@ -2,47 +2,50 @@ import styled from "styled-components";
 import { useState } from "react";
 import { useNavigate, useLocation } from "react-router-dom";
 
-import { appColor, height_size } from "../../utils/style";
+import { appColor, height_size, width_size } from "../../utils/style";
 import Button from "./Button";
+import MainIcon from "../../assets/main.svg";
+import { Wrapper } from "./Wrapper";
+import { BorderColumn } from "./Border";
+import { MediumText } from "./Text";
+import HeaderMenu from "./HeaderMenu";
 
 export default () => {
   const navigate = useNavigate();
-  const location = useLocation();
-  const [isHomeActive, setIsHomeActive] = useState(
-    location.pathname === "/home"
-  );
 
-  const homeOnClick = () => {
+  // TODO: should be manage by redux
+  const [target, setTarget] = useState<string | null>(null); //target real estate
+
+  const testClick = () => {
     navigate("/home");
-    setIsHomeActive(true);
-  };
-  const searchOnClick = () => {
-    navigate("/search");
-    setIsHomeActive(false);
+    setTarget((prevTarget) => (prevTarget ? null : "청학아파트재건축정비사업"));
   };
 
   return (
     <HeaderWrapper>
-      <Button
-        onClick={homeOnClick}
-        transparent={true}
-        textOption={{
-          text: "재개발/재건축 구역 정보",
-          color: isHomeActive ? "purple" : "black",
-          size: 16,
-          weight: "medium",
-        }}
-      />
-      <Button
-        onClick={searchOnClick}
-        transparent={true}
-        textOption={{
-          text: "유사도 검색",
-          color: isHomeActive ? "black" : "purple",
-          size: 16,
-          weight: "medium",
-        }}
-      />
+      <Wrapper direction="row" gap={28} center={true}>
+        <Button
+          onClick={testClick}
+          transparent={true}
+          textOption={{
+            text: "",
+            color: "black",
+            size: 16,
+            weight: "medium",
+          }}
+          iconOption={{ icon: MainIcon, width: 27, height: 27 }}
+        />
+        <BorderColumn width={1} length={47} color="grayLight" />
+        <MediumText size={16} color={target ? "black" : "grayLight"}>
+          {target ?? "사업을 선택하세요"}
+        </MediumText>
+      </Wrapper>
+
+      {target && <HeaderMenu />}
+
+      <Wrapper direction="row" width={135}>
+        {"계정정보"}
+      </Wrapper>
     </HeaderWrapper>
   );
 };
@@ -54,9 +57,11 @@ const HeaderWrapper = styled.div`
   background-color: ${appColor.white};
   border-bottom: 1px solid ${appColor.grayLight};
 
+  box-sizing: border-box;
   display: flex;
   flex-direction: row;
   align-items: center;
-  justify-content: center;
-  gap: 3vw;
+  justify-content: space-between;
+
+  padding: 0 ${width_size(40)} 0 ${width_size(30)};
 `;
