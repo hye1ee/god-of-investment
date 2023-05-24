c286e723b687d6398ed35c679c8caeb393068a62
Hyewon Lee <88485008+hye1ee@users.noreply.github.com>
2023-05-25 01:00:03 +0900

    Page issue (#27)
    * [feat] Implement issue page boxes partially
    
    * [feat] Change view style
    
    * [style] Change page layout

--- src/pages/issues/components/RelateBox.tsx
+++ src/pages/issues/components/RelateBox.tsx
@@ -1,15 +1,42 @@
+import React from "react";
+import { useSelector } from "react-redux";
 import { TextBox } from "../../components/Text";
 import { Wrapper } from "../../components/Wrapper";
 import BoxLayout from "../../detail/components/BoxLayout";
+import { useEffect, useState } from "react";
+import { RootState } from "../../../states/store";
+import { getConstructionKeywords } from "../../../apis/detail";
+import Tag from "../../components/Tag";
 
 const RelateBox = () => {
+  const [relateNames, setRelateNames] = useState<string[]>([]);
+  const consId = useSelector((state: RootState) => state.target.id);
+
+  const asyncWrapper = async () => {
+    if (consId == null) return;
+    setRelateNames(await getConstructionKeywords(consId));
+  };
+  useEffect(() => {
+    asyncWrapper();
+  }, []);
+
   return (
-    <BoxLayout width={362} color="white" title="연관 사업">
-      <Wrapper direction="column" gap={10}>
-        <RelateTag title="성수동아 재건축" />
-        <RelateTag title="성수동아 재건축" />
-        <RelateTag title="성수동아 재건축" />
-        <RelateTag title="성수동아 재건축" />
+    <BoxLayout width={1202} color="white" title="키워드">
+      <Wrapper direction="row" gap={10} width={1100} wrap={true}>
+        {relateNames.map((name, idx) => (
+          <Tag
+            key={idx}
+            textOption={{
+              text: name,
+              weight: "regular",
+              color: "black",
+              size: 13,
+            }}
+            radius={17}
+            paddingOption={{ width: 20, height: 5 }}
+            color="redLight"
+          />
+        ))}
       </Wrapper>
     </BoxLayout>
   );
