2638464e7673ef1f2f4131763ec6f11b613a2f15
hye1ee <hyewon0809@kaist.ac.kr>
2023-05-25 12:53:14 +0900

    [feat] Implement issue box

--- src/pages/issues/components/IssueBox.tsx
+++ src/pages/issues/components/IssueBox.tsx
@@ -1,65 +1,66 @@
-import Tag from "../../components/Tag";
+import { useEffect, useState } from "react";
+import { getIssues } from "../../../apis/issue";
 import { Wrapper } from "../../components/Wrapper";
 import BoxLayout from "../../detail/components/BoxLayout";
 import { IssueContainer } from "./IssueContainer";
-
-const testProps = {
-  title: "잠실·개포 아파트 석달새 2억 껑충…강남 전셋값 바닥쳤나",
-  date: "2023.05.11",
-  src: "중앙 일보",
-  url: "https://www.joongang.co.kr/article/25161726",
-};
+import { RootState } from "../../../states/store";
+import { useDispatch, useSelector } from "react-redux";
+import { RegularText } from "../../components/Text";
+import { updateIssue } from "../../../states/issueSlice";
 
 const IssueBox = () => {
+  const [issues, setIssues] = useState<any[]>([]);
+  const issueId = useSelector((state: RootState) => state.issue.id);
+  const consId = useSelector((state: RootState) => state.target.id);
+  const dispatch = useDispatch();
+
+  const asyncWrapper = async () => {
+    dispatch(updateIssue({ id: null, summary: null }));
+
+    if (!consId) return;
+    let newIssues = await getIssues(consId);
+    newIssues.forEach((issue: any) => {
+      issue.pubDate = new Date(issue.pubDate);
+    });
+
+    newIssues = newIssues.sort((a: any, b: any) => b.pubDate - a.pubDate);
+    setIssues(newIssues);
+  };
+
+  const setIssue = (id: number) => {
+    const targetIssue = issues.find((issue) => issue.id == id);
+    dispatch(updateIssue({ id, summary: targetIssue.gpt_summary }));
+  };
+
+  useEffect(() => {
+    asyncWrapper();
+  }, []);
+
   return (
     <BoxLayout width={362} color="white" title="사업 연관 이슈">
-      <Wrapper direction="column" gap={10}>
-        <Tag
-          textOption={{
-            text: "3월",
-            weight: "regular",
-            color: "black",
-            size: 13,
-          }}
-          radius={17}
-          paddingOption={{ width: 20, height: 5 }}
-          color="blueLight"
-        />
-
-        <IssueContainer {...testProps} />
-        <IssueContainer {...testProps} />
-      </Wrapper>
-      <Wrapper direction="column" gap={10}>
-        <Tag
-          textOption={{
-            text: "4월",
-            weight: "regular",
-            color: "black",
-            size: 13,
-          }}
-          radius={17}
-          paddingOption={{ width: 20, height: 5 }}
-          color="blueLight"
-        />
-
-        <IssueContainer {...testProps} />
-        <IssueContainer {...testProps} />
-      </Wrapper>
-      <Wrapper direction="column" gap={10}>
-        <Tag
-          textOption={{
-            text: "5월",
-            weight: "regular",
-            color: "black",
-            size: 13,
-          }}
-          radius={17}
-          paddingOption={{ width: 20, height: 5 }}
-          color="blueLight"
-        />
-
-        <IssueContainer {...testProps} />
-        <IssueContainer {...testProps} />
+      <Wrapper direction="column" gap={10} width="full">
+        {issues.length == 0 && (
+          <Wrapper direction="row" width="full" center={true}>
+            <RegularText size={13} color="gray">
+              이슈 연동 준비 중
+            </RegularText>
+          </Wrapper>
+        )}
+        {issues.map((issue) => (
+          <IssueContainer
+            key={issue.id}
+            src={issue.media}
+            url={issue.url}
+            title={issue.title}
+            date={[
+              issue.pubDate.getFullYear(),
+              issue.pubDate.getMonth(),
+              issue.pubDate.getDate(),
+            ].join("-")}
+            active={issueId == issue.id}
+            onClick={() => setIssue(issue.id)}
+          />
+        ))}
       </Wrapper>
     </BoxLayout>
   );
--- src/states/store.ts
+++ src/states/store.ts
@@ -3,9 +3,10 @@ import { default as search } from './searchSlice';
 import { default as target } from './targetSlice';
 import { default as simulation } from './simulationSlice';
 import { default as user } from './userSlice';
+import { default as issue } from './issueSlice';
 
 export const store = configureStore({
-  reducer: { search, target, simulation, user },
+  reducer: { search, target, simulation, user, issue },
 });
 
 export type RootState = ReturnType<typeof store.getState>;
